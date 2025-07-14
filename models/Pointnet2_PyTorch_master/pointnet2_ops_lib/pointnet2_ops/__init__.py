5a282e1405b3d4ca5aea0d8e7356c13fef9b73a6
Meng Xianhui <91462582+SEU-mxh@users.noreply.github.com>
2025-07-14 20:05:14 +0800

    Delete models/Pointnet2_PyTorch_master directory

--- models/Pointnet2_PyTorch_master/pointnet2_ops_lib/pointnet2_ops/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/home/mxh24/codes/PPF_Tracker_release/models/Pointnet2_PyTorch_master')
-sys.path.append('/home/mxh24/codes/PPF_Tracker_release/models/Pointnet2_PyTorch_master/pointnet2_ops_lib')
-import pointnet2_ops.pointnet2_modules
-import pointnet2_ops.pointnet2_utils
-from pointnet2_ops._version import __version__
